c94f08152a0e317275627218f538d55e40d1085e
bczhc <bczhc0@126.com>
2021-03-13 20:00:39 +0800

    initial commit

---
+++ src/main/java/pers/zhc/spring/test/SpringDemoApplication.java
@@ -0,0 +1,11 @@
+package pers.zhc.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+}
